a635ed92c5c4730dd3a79990c55166f0b59a5273
gbahugun <22356761+gbahugun@users.noreply.github.com>
2023-10-16 22:45:00 -0700

    Updated sleep_ref.txt. Changed some spacing in thread.c.

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -71,10 +71,10 @@ static struct thread *running_thread (void);
 static struct thread *next_thread_to_run (void);
 static void init_thread (struct thread *, const char *name, int priority);
 static bool is_thread (struct thread *) UNUSED;
-                                        static void *alloc_frame (struct thread *, size_t size);
-                                        static void schedule (void);
-                                        void thread_schedule_tail (struct thread *prev);
-                                        static tid_t allocate_tid (void);
+static void *alloc_frame (struct thread *, size_t size);
+static void schedule (void);
+void thread_schedule_tail (struct thread *prev);
+static tid_t allocate_tid (void);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
